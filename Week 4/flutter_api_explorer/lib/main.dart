9838919865888d9234b01ff68591477fa842667f
DevExplorerr <saadj4775@gmail.com>
2025-09-02 13:02:29 +0500

    Flutter Api Explorer

--- Week 4/flutter_api_explorer/lib/main.dart
+++ Week 4/flutter_api_explorer/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Flutter Api Explorer",
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
